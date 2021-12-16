c7027d6103055ac1e47d455fd17dd3f250d69c9a
Pedro Carmona <wpcarmona@unicesar.edu.co>
2021-12-15 22:58:49 -0500

    agregado de ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageListComponent } from './image-list/image-list.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageListComponent,
+    UserInfoComponent,
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: false,
+  PIXABAY_API_URL: "https://pixabay.com/api/?key=13119377-fc7e10c6305a7de49da6ecb25",
 };
